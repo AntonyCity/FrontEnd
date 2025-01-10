8211e84b22e4a3a62fe890d321d1b40f588397c7
Charles-Chrismann <charles.chrismann@free.fr>
2025-01-10 14:17:13 +0100

    chore: configuration build -> dist/

--- FrontEnd/src/views/login.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Login() {
-    return (React.createElement(React.Fragment, null,
-        React.createElement("p", null, "a")));
-}
-exports.default = Login;
--- FrontEnd/src/views/offres.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Offres() {
-    return (React.createElement(React.Fragment, null));
-}
-exports.default = Offres;
